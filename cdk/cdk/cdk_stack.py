9e40578fcd98992fd553cbefbf5e5a33f9e22819
AkiraManga <salvatore.m98@live.it>
2025-09-16 20:15:53 +0200

    Aggiunta del bucket in stack

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
+    aws_iam as iam,
+    RemovalPolicy,
 )
 from constructs import Construct
 
@@ -10,10 +11,20 @@ class CdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Rimuovi l'accesso pubblico
+        website_bucket = s3.Bucket(self, "FrontendBucket",
+            bucket_name="il-tuo-sito-web-personale-qui", # Deve essere unico globalmente
+            website_index_document="index.html",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL, # Blocca tutto l'accesso pubblico
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Crea una policy per consentire l'accesso al tuo utente IAM
+        website_bucket.add_to_resource_policy(
+            iam.PolicyStatement(
+                actions=["s3:GetObject"],
+                resources=[website_bucket.arn_for_objects("*")],
+                principals=[iam.ArnPrincipal("arn:aws:iam::384981131796:user/Salvatore")] # Sostituisci con il tuo ARN
+            )
+        )
