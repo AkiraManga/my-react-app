d929218dfad3b763a6abac66e1ebc7d15d4ea7cb
AkiraManga <salvatore.m98@live.it>
2025-09-17 23:02:41 +0200

    Aggiunto stack CDK con S3, DynamoDB, Lambda, API Gateway e Cognito

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -4,26 +4,30 @@ from aws_cdk import (
     aws_iam as iam,
     aws_lambda as _lambda,
     aws_apigateway as apigw,
+    aws_dynamodb as dynamodb,
+    aws_cognito as cognito,
     RemovalPolicy,
-    aws_cognito as cognito
 )
 from constructs import Construct
+
 
 class CdkStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Rimuovi l'accesso pubblico
-        website_bucket = s3.Bucket(self, "FrontendBucket",
-            bucket_name="rate-your-music101",  # Deve essere unico globalmente
+        # --------------------------
+        # S3 Bucket per il frontend
+        # --------------------------
+        website_bucket = s3.Bucket(
+            self, "FrontendBucket",
+            bucket_name="rate-your-music101",  # deve essere unico globalmente
             website_index_document="index.html",
             block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
             removal_policy=RemovalPolicy.DESTROY,
-            auto_delete_objects=True
+            auto_delete_objects=True,
         )
 
-        # Crea una policy per consentire l'accesso al tuo utente IAM
         website_bucket.add_to_resource_policy(
             iam.PolicyStatement(
                 actions=["s3:GetObject"],
@@ -32,14 +36,9 @@ class CdkStack(Stack):
             )
         )
 
-        # Lambda function (codice in lambda/app.py, funzione "handler")
-        lambda_fn = _lambda.Function(
-            self, "MyLambda",
-            runtime=_lambda.Runtime.PYTHON_3_12,
-            handler="app.handler",  # app.py → funzione handler
-            code=_lambda.Code.from_asset("lambda")
-        )
-                # Cognito User Pool
+        # --------------------------
+        # Cognito User Pool
+        # --------------------------
         user_pool = cognito.UserPool(
             self, "RateYourMusicUserPool",
             self_sign_up_enabled=True,
@@ -53,29 +52,106 @@ class CdkStack(Stack):
             removal_policy=RemovalPolicy.DESTROY
         )
 
-        # Client per il frontend React
-        user_pool_client = cognito.UserPoolClient(
-            self, "RateYourMusicClient",
-            user_pool=user_pool,
+        user_pool_client = user_pool.add_client(
+            "RateYourMusicClient",
             generate_secret=False,
-            auth_flows=cognito.AuthFlow(user_password=True)
+            auth_flows=cognito.AuthFlow(user_password=True),
+            o_auth=cognito.OAuthSettings(
+                flows=cognito.OAuthFlows(
+                    authorization_code_grant=True,
+                    implicit_code_grant=True
+                ),
+                scopes=[
+                    cognito.OAuthScope.OPENID,
+                    cognito.OAuthScope.EMAIL,
+                    cognito.OAuthScope.PROFILE,
+                ],
+                callback_urls=["https://jwt.io"],
+                logout_urls=["https://jwt.io"],
+            ),
         )
 
-        # Authorizer per API Gateway
+        domain = user_pool.add_domain(
+            "RateYourMusicDomain",
+            cognito_domain=cognito.CognitoDomainOptions(
+                domain_prefix="rateyourmusic101"  # deve essere univoco a livello AWS
+            ),
+        )
+
+        # --------------------------
+        # DynamoDB
+        # --------------------------
+        users_table = dynamodb.Table(
+            self, "UsersTable",
+            partition_key={"name": "user_id", "type": dynamodb.AttributeType.STRING},
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        albums_table = dynamodb.Table(
+            self, "AlbumsTable",
+            partition_key={"name": "album_id", "type": dynamodb.AttributeType.STRING},
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        ratings_table = dynamodb.Table(
+            self, "RatingsTable",
+            partition_key={"name": "album_id", "type": dynamodb.AttributeType.STRING},
+            sort_key={"name": "user_id", "type": dynamodb.AttributeType.STRING},
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        # --------------------------
+        # Lambda Functions (Docker)
+        # --------------------------
+        albums_fn = _lambda.DockerImageFunction(
+            self, "AlbumsLambda",
+            code=_lambda.DockerImageCode.from_image_asset("lambda/albums"),
+        )
+        albums_table.grant_read_data(albums_fn)
+
+        vote_fn = _lambda.DockerImageFunction(
+            self, "VoteLambda",
+            code=_lambda.DockerImageCode.from_image_asset("lambda/vote"),
+        )
+        ratings_table.grant_read_write_data(vote_fn)
+        albums_table.grant_read_write_data(vote_fn)
+
+        favorites_fn = _lambda.DockerImageFunction(
+            self, "FavoritesLambda",
+            code=_lambda.DockerImageCode.from_image_asset("lambda/favorites"),
+        )
+        users_table.grant_read_write_data(favorites_fn)
+
+        # --------------------------
+        # API Gateway + Cognito Authorizer
+        # --------------------------
         authorizer = apigw.CognitoUserPoolsAuthorizer(
             self, "RateYourMusicAuthorizer",
-            cognito_user_pools=[user_pool]
-        )
-        # API Gateway protetto da Cognito
-        api = apigw.LambdaRestApi(
-            self, "RateYourMusicApi",
-            handler=lambda_fn,
-            proxy=True,
-            default_method_options={
-                "authorizer": authorizer,
-                "authorization_type": apigw.AuthorizationType.COGNITO
-            }
+            cognito_user_pools=[user_pool],
         )
 
+        api = apigw.RestApi(self, "RateYourMusicApi")
 
+        api.root.add_resource("albums").add_method(
+            "GET",
+            apigw.LambdaIntegration(albums_fn),
+            authorizer=authorizer,
+            authorization_type=apigw.AuthorizationType.COGNITO,
+        )
 
+        api.root.add_resource("vote").add_method(
+            "POST",
+            apigw.LambdaIntegration(vote_fn),
+            authorizer=authorizer,
+            authorization_type=apigw.AuthorizationType.COGNITO,
+        )
+
+        api.root.add_resource("favorites").add_method(
+            "POST",
+            apigw.LambdaIntegration(favorites_fn),
+            authorizer=authorizer,
+            authorization_type=apigw.AuthorizationType.COGNITO,
+        )
---
+++ cdk/lambda/vote/app.py
@@ -0,0 +1,8 @@
+import json
+
+def handler(event, context):
+    body = json.loads(event.get("body", "{}"))
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"message": "Vote Lambda in Docker funziona!", "data": body})
+    }
