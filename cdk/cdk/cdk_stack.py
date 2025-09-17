0367a284145387b76611749c72965b68da56d8e1
AkiraManga <salvatore.m98@live.it>
2025-09-17 16:15:38 +0200

    Aggiunta lambda e API gateway

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -2,6 +2,8 @@ from aws_cdk import (
     Stack,
     aws_s3 as s3,
     aws_iam as iam,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
     RemovalPolicy,
 )
 from constructs import Construct
@@ -13,9 +15,9 @@ class CdkStack(Stack):
 
         # Rimuovi l'accesso pubblico
         website_bucket = s3.Bucket(self, "FrontendBucket",
-            bucket_name="rate-your-music101", # Deve essere unico globalmente
+            bucket_name="rate-your-music101",  # Deve essere unico globalmente
             website_index_document="index.html",
-            block_public_access=s3.BlockPublicAccess.BLOCK_ALL, # Blocca tutto l'accesso pubblico
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
             removal_policy=RemovalPolicy.DESTROY,
             auto_delete_objects=True
         )
@@ -25,6 +27,21 @@ class CdkStack(Stack):
             iam.PolicyStatement(
                 actions=["s3:GetObject"],
                 resources=[website_bucket.arn_for_objects("*")],
-                principals=[iam.ArnPrincipal("arn:aws:iam::384981131796:user/Salvatore")] # Sostituisci con il tuo ARN
+                principals=[iam.ArnPrincipal("arn:aws:iam::384981131796:user/Salvatore")]
             )
+        )
+
+        # Lambda function (codice in lambda/app.py, funzione "handler")
+        lambda_fn = _lambda.Function(
+            self, "MyLambda",
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            handler="app.handler",  # app.py → funzione handler
+            code=_lambda.Code.from_asset("lambda")
+        )
+
+        # API Gateway REST che punta alla Lambda
+        api = apigw.LambdaRestApi(
+            self, "RateYourMusicApi",
+            handler=lambda_fn,
+            proxy=True
         )
